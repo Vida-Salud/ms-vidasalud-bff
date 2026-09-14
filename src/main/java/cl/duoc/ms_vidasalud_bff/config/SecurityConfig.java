4319977bb9a84a9fc8a407f45d36001cddbb786b
astogeee510 <pabloastroza671@gmail.com>
2026-09-14 02:30:38 -0300

    CORS origen EC2

--- src/main/java/cl/duoc/ms_vidasalud_bff/config/SecurityConfig.java
+++ src/main/java/cl/duoc/ms_vidasalud_bff/config/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOrigins(List.of("http://localhost:4200", "https://54.85.92.234"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
 
